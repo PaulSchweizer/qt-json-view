5bde8690e98a1949afbbe9c6e5c3c3337e1c9c02
Paul Schweizer <paulschweizer@gmx.net>
2023-05-17 08:43:20 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="qt-json-view",
+    version="0.0.1",
+    author="Paul Schweizer",
+    description="A Qt widget for viewing json files as tree models",
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+)
